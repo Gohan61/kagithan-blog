e74400cb5d871e90c988ffd207741e824957a736
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-12-25 18:33:05 +0100

    Feat: set/use routes as middleware

--- kagithan-backend/src/app.ts
+++ kagithan-backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
+import { router } from "./routes";
 
 export const app = express();
 
@@ -9,6 +10,8 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   res.status(err.status | 500);
   res.send({ error: err });
 });
+
+app.use("/", router);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
