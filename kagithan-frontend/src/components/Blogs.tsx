942536c368f7b59bfa2daebf03015d849b6818cf
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2025-04-03 16:44:14 +0200

    Refactor: fetch blogs from worker + convert
    * As the worker returns a ReadableStream it is converted accordingly to
    serve either the blog or an error message

--- kagithan-frontend/src/components/Blogs.tsx
+++ kagithan-frontend/src/components/Blogs.tsx
@@ -1,24 +1,51 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import posts from "../posts";
+const CLOUDFLARE_WORKER_API = import.meta.env.VITE_CLOUDFLARE_WORKER_API;
 
 export default function Blogs() {
+  const [posts, setPosts] = useState<string[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch(CLOUDFLARE_WORKER_API, {
+      method: "GET",
+      mode: "cors",
+    }).then(async (res) => {
+      const reader = res.body?.getReader();
+      const result = await reader?.read();
+      const decoder = new TextDecoder();
+
+      if (res.status === 200 && result) {
+        const text = decoder.decode(result.value);
+        setPosts(text.split(","));
+        return;
+      }
+
+      const errorMessage = decoder.decode(result?.value);
+      setError(errorMessage);
+    });
+  }, []);
+
   return (
     <div className="flex flex-col gap-2 w-[300px] md:w-[600px] ml-auto mr-auto">
       <h2 className="text-xl font-semibold">Blogs</h2>
       <ul className="mt-3">
-        {posts.map((post) => {
-          return (
-            <li key={post.file} className="underline mb-2">
-              <Link
-                to={`/blog/${post.file}`}
-                className="visited:text-purple-800"
-              >
-                {post.title}
-              </Link>
-            </li>
-          );
-        })}
+        {posts
+          ? posts.map((post) => {
+              return (
+                <li key={post} className="underline mb-2">
+                  <Link
+                    to={`/blog/${post}`}
+                    className="visited:text-purple-800"
+                  >
+                    {post}
+                  </Link>
+                </li>
+              );
+            })
+          : ""}
       </ul>
+      {error ? <p>{error}</p> : ""}
     </div>
   );
 }
--- kagithan-frontend/src/components/SingleBlog.tsx
+++ kagithan-frontend/src/components/SingleBlog.tsx
@@ -1,30 +1,42 @@
 import { useEffect, useState } from "react";
 import ReactMarkDown from "react-markdown";
 import { useParams } from "react-router-dom";
+const CLOUDFLARE_WORKER_API = import.meta.env.VITE_CLOUDFLARE_WORKER_API;
 
 export default function SingleBlog() {
   const { file } = useParams();
   const [content, setContent] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    (async () => {
-      try {
-        const response = await fetch(`/content/${file}`);
-        if (!response.ok) {
-          throw new Error("Failed to fetch Markdown file");
+    (() => {
+      fetch(`${CLOUDFLARE_WORKER_API}/${file}`, {
+        mode: "cors",
+        method: "GET",
+      }).then(async (res) => {
+        const reader = res.body?.getReader();
+        const result = await reader?.read();
+        const decoder = new TextDecoder();
+
+        if (res.status === 200 && result) {
+          const text = decoder.decode(result.value);
+          setContent(text);
+          return;
         }
-        const text = await response.text();
-        setContent(text);
-      } catch (error) {
-        console.error(error);
-      }
+
+        const errorMessage = decoder.decode(result?.value);
+        setError(errorMessage);
+      });
     })();
   }, [file]);
 
   return (
-    <ReactMarkDown
-      className="markdown flex flex-col gap-2 w-[300px] md:w-[600px] ml-auto mr-auto"
-      children={content}
-    ></ReactMarkDown>
+    <>
+      <ReactMarkDown
+        className="markdown flex flex-col gap-2 w-[300px] md:w-[600px] ml-auto mr-auto"
+        children={content}
+      ></ReactMarkDown>
+      {error ? <p>{error}</p> : ""}
+    </>
   );
 }
