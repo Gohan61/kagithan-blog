cf16fb3c0797c50773471f78e83a1cc17c4ce378
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2025-04-03 16:48:56 +0200

    Del: remove backend config from main branch
    * Backend config is not needed for worker setup

--- kagithan-backend/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllBlogs, getBlog } from "../controllers/getBlogs";
-
-export const router = express.Router();
-
-router.get("/blogs", getAllBlogs);
-
-router.get("/:blogTitle", getBlog);
--- kagithan-backend/types/types.ts
+++
@@ -1,6 +0,0 @@
-type post = {
-  title: string;
-  file: string;
-};
-
-export type postsType = post[];
